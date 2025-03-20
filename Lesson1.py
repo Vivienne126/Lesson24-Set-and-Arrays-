b3d2de6b28f310136119900d1366bc6eb57a3d89
Vivienne126 <vivienne.singh@cns.ac.in>
2025-03-20 20:57:12 +0530

    New

---
+++ Lesson1.py
@@ -0,0 +1,14 @@
+my_set={1,2,3,4}
+print(my_set)
+
+my_set2={1,"Hello" , (1,2,3,4)}
+print(my_set2)
+
+my_set3={1,2,1,4,3,2,4,1,3,1,2,}
+print(my_set3)
+
+my_set4=set([1,2,1,2,3,2,3,4,1,4,1,2])
+print(my_set4)
+
+my_set.pop()
+print(my_set)
